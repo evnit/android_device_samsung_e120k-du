ca2330a26b60c220977915a8a046fc7cbb12c131
evnit <evnithn@gmail.com>
2014-05-11 04:17:54 -0400

    Update du.mk

--- du.mk
+++ du.mk
@@ -3,11 +3,11 @@ $(call inherit-product, device/samsung/e120k/full_e120k.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/du/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/version.mk)
+$(call inherit-product, vendor/du/config/packages.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120K TARGET_DEVICE=SHV-E120K BUILD_FINGERPRINT="samsung/SHV-E120K/SHV-E120K:4.4.2/KOT49E/KKMK1:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120K-user 4.4.2 KOT49E KKMK1 release-keys"
 
@@ -16,4 +16,3 @@ TARGET_SCREEN_WIDTH := 720
 
 PRODUCT_NAME := du_e120k
 PRODUCT_DEVICE := e120k
-
